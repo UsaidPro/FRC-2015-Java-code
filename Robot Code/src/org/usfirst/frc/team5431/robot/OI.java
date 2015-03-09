91976421d5d9cb1fa155dcf64927d6bf6b79a8fc
David Smerkous <smerkousdavid@gmail.com>
2015-03-08 20:45:26 -0500

    Description update on IO.java
    Added comments on mappings and control settings

--- Robot Code/src/org/usfirst/frc/team5431/robot/OI.java
+++ Robot Code/src/org/usfirst/frc/team5431/robot/OI.java
@@ -1,16 +1,37 @@
-//THIS IS WHERE WE MAP BUTTONS - apparently (Usaid)
-
 package org.usfirst.frc.team5431.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team5431.robot.commands.ExampleCommand;
-
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
+////// This class is for connecting the computer to robot with the joystick values
 public class OI {
-    //// CREATING BUTTONS
+    ////////////////////////////////THE MAPPING OF THE BUTTONS/AXIS(Xbox Controller)
+	//////////// All axis are a value from (-1 to 1) some are different and most are inverted
+	///// button x = 3
+	///// button a = 1
+	///// button b = 2
+	///// button y = 4
+	///// button RB = 6 (Right bumper)
+	///// button LB = 5 (Left bumper)
+	///// button for press down on sticks are 9(topleft) and 10(bottom right)
+	//////////////////// These are both one axis RT and LT
+	///// axis RT (right trigger) = 3 (0 to -1)
+	///// axis LT (left trigger) = 3 (0 to 1)
+	///// axis TLX  (top left x value) = 1
+	///// axis TLY  (top left y value) = 2
+	///// axis BRX  (Bottom right x value) = 4
+	///// axis BRY  (Bottom right y value) = 5
+	///// POV(plus sign looking thing x value only is) = 6
+	////////////////////////////////THE MAPPING OF THE BUTTONS/AXIS(Logitech Gamepad)
+	///// button trigger = 1
+	///// buttons everywhere else are numbered 1 thru 6
+	////////////////
+	///// Main joystick (x axis) = 1
+	///// Main joystick (y axis) = 2
+	///// Main joystick (z axis) = 3
+	///// Slider on the bottom back(only one axis which is y) = 4
+	///// POV(little thing on top x) = 5
+	///// POV(little thing on top y) = 6
+    /////////////////////////////////////// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
