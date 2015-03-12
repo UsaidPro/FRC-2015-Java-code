b0604359f6c7fa44dbffe0f11d69319d6de3f59b
David <smerkousdavid@gmail.com>
2015-03-12 16:42:24 -0600

    Autonomous selection pyhsical switches added

--- Robot Code/src/org/usfirst/frc/team5431/robot/commands/Autonomous.java
+++ Robot Code/src/org/usfirst/frc/team5431/robot/commands/Autonomous.java
@@ -1,10 +1,17 @@
 package org.usfirst.frc.team5431.robot.commands;
 import org.usfirst.frc.team5431.robot.Robot;
+import org.usfirst.frc.team5431.robot.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
+
 import org.usfirst.frc.team5431.robot.subsystems.Motorcontrol;
+
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 public class Autonomous extends Command {
-
+	DigitalInput on_off;
+	DigitalInput auto1;
+	DigitalInput auto2;
+	DigitalInput auto3;
     public Autonomous() {
         requires(Robot.Motorcontrol);
     }
@@ -12,10 +19,39 @@ public class Autonomous extends Command {
     protected void initialize() {
     	Motorcontrol.stopdrive();
     	Motorcontrol.stoplift();
+    	on_off = new DigitalInput(RobotMap.auto_onoff);
+    	auto1 = new DigitalInput(RobotMap.auto1);
+    	auto2 = new DigitalInput(RobotMap.auto2);
+    	auto3 = new DigitalInput(RobotMap.auto3);
     }
     
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if(on_off.get() == true){
+    		if(auto1.get() == true && auto2.get() == false && auto3.get()==false){
+    			Motorcontrol.forward();
+    			Timer.delay(1);
+    			Motorcontrol.stopdrive();
+    		}else if(auto1.get() == false && auto2.get() == true && auto3.get() == false){
+    			Motorcontrol.backward();
+    			Timer.delay(1);
+    			Motorcontrol.stopdrive();
+    		}else if(auto1.get() == false && auto2.get() == false && auto3.get() == true){
+    			Motorcontrol.backward();
+    			Timer.delay(1);
+    			Motorcontrol.forward();
+    			Timer.delay(0.5);
+    			Motorcontrol.stopdrive();
+    		}else{
+    			Motorcontrol.forward();
+    			Timer.delay(1);
+    			Motorcontrol.stopdrive();
+    		}
+    	}else{
+    		Motorcontrol.stopdrive();
+    		Timer.delay(0.05);
+    		Motorcontrol.stoplift();
+    	}
     Motorcontrol.forward();
     Timer.delay(1);
     Motorcontrol.backward();
